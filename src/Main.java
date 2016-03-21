d3ed0ae95d3728e06effc7cdbe0d3939eba10dfc
k00770 <k00770@E4-20.readingschool.reading.sch.uk>
2016-03-21 09:00:05 +0000

    AI works! (I think) + You can tease the opponent

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,7 @@ public class Main {
 
 	void yourMumDis(Rapper[] rappers, Dis[] disses, int rappersBeaten) {
 		Random r = new Random();
-		int hypeGained = r.nextInt(5);
+		int hypeGained = r.nextInt(6);
 
 		System.out.println("You rapped about his mum");
 		System.out.println("You gained " + hypeGained + "% of the crowd hype");
@@ -44,12 +44,29 @@ public class Main {
 		AITurn(rappers, disses, rappersBeaten);
 	}
 
+	void yourMumDisAI(Rapper[] rappers, Dis[] disses, int rappersBeaten) {
+		Random r = new Random();
+		int hypeGained = r.nextInt(6);
+
+		System.out.println(rappers[rappersBeaten].name
+				+ " rapped about your mum");
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("He gained " + hypeGained + "% of the crowd hype");
+
+		rappers[rappersBeaten].hypePoints += hypeGained;
+
+	}
+
 	void callName(Rapper[] rappers, Dis[] disses, int rappersBeaten) {
 		Random r = new Random();
 		String adjective;
 		String noun;
 		int count;
-		count = r.nextInt(10) + 1;
+		count = r.nextInt(10);
 
 		try {
 			BufferedReader adj = new BufferedReader(new FileReader(
@@ -65,7 +82,7 @@ public class Main {
 
 			}
 
-			count = r.nextInt(10) + 1;
+			count = r.nextInt(10);
 
 			for (int i = 0; i < count; i++) {
 				noun = nou.readLine();
@@ -78,14 +95,73 @@ public class Main {
 			adj.close();
 			nou.close();
 
-			int hypeGained = r.nextInt(21) + 10;
+			int hypeGained = r.nextInt(11);
 
 			System.out.println("You gained " + hypeGained
 					+ "% of the crowd hype");
 
 			rappers[rappersBeaten].hypePoints -= hypeGained;
-			
+
 			AITurn(rappers, disses, rappersBeaten);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	void callNameAI(Rapper[] rappers, Dis[] disses, int rappersBeaten) {
+		Random r = new Random();
+		String adjective;
+		String noun;
+		int count;
+		count = r.nextInt(10);
+
+		try {
+			BufferedReader adj = new BufferedReader(new FileReader(
+					"adjectives.txt"));
+			BufferedReader nou = new BufferedReader(new FileReader("nouns.txt"));
+
+			for (int i = 0; i < count; i++) {
+				adjective = adj.readLine();
+
+				if (i == count - 1) {
+					System.out.print("You got called a " + adjective + " ");
+				}
+
+			}
+
+			count = r.nextInt(10);
+
+			for (int i = 0; i < count; i++) {
+				noun = nou.readLine();
+
+				if (i == count - 1) {
+					System.out.println(noun);
+				}
+
+			}
+			adj.close();
+			nou.close();
+			
+			try {
+				Thread.sleep(1500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			int hypeGained = r.nextInt(11);
+
+			System.out.println(rappers[rappersBeaten].name + " gained "
+					+ hypeGained + "% of the crowd hype");
+			
+			try {
+				Thread.sleep(1500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			rappers[rappersBeaten].hypePoints += hypeGained;
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -95,8 +171,81 @@ public class Main {
 
 	void teaseDis(Rapper[] rappers, Dis[] disses, int rappersBeaten) {
 
-		
-		AITurn(rappers, disses, rappersBeaten);
+		String tease;
+		Random r = new Random();
+		int count;
+		count = r.nextInt(4);
+
+		try {
+			BufferedReader teaseR = new BufferedReader(new FileReader(
+					"teases.txt"));
+
+			for (int i = 0; i < count; i++) {
+				tease = teaseR.readLine();
+
+				if (i == count - 1) {
+					System.out.println("You told "
+							+ rappers[rappersBeaten].name + " he " + tease);
+				}
+			}
+			teaseR.close();
+
+			int hypeGained = r.nextInt(21);
+
+			System.out.println("You gained " + hypeGained
+					+ "% of the crowd hype");
+
+			rappers[rappersBeaten].hypePoints -= hypeGained;
+
+			AITurn(rappers, disses, rappersBeaten);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	void teaseDisAI(Rapper[] rappers, Dis[] disses, int rappersBeaten) {
+
+		String tease;
+		Random r = new Random();
+		int count;
+		count = r.nextInt(4);
+
+		try {
+			BufferedReader teaseR = new BufferedReader(new FileReader(
+					"teasesAI.txt"));
+
+			for (int i = 0; i < count; i++) {
+				tease = teaseR.readLine();
+
+				if (i == count - 1) {
+					System.out.println("He said to you that you " + tease);
+				}
+			}
+			teaseR.close();
+			
+			try {
+				Thread.sleep(1500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			int hypeGained = r.nextInt(21);
+
+			System.out.println("He gained " + hypeGained
+					+ "% of the crowd hype");
+			
+			try {
+				Thread.sleep(1500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			rappers[rappersBeaten].hypePoints += hypeGained;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	void getMenuChoice(int choice, Rapper[] rappers, Dis[] disses) {
@@ -117,36 +266,37 @@ public class Main {
 		boolean beaten = false;
 		boolean won;
 
-			System.out.println("You have been challenged by "
-					+ rappers[rappersBeaten].name);
-			System.out.println("He has " + rappers[rappersBeaten].hypePoints
-					+ "% of the crowd's hype");
-			System.out.println("Dis him up to get 100% of the hype");
+		System.out.println("You have been challenged by "
+				+ rappers[rappersBeaten].name);
+		System.out.println("He has " + rappers[rappersBeaten].hypePoints
+				+ "% of the crowd's hype");
+		System.out.println("Dis him up to get 100% of the hype");
 
-			won = false;
+		won = false;
 
-			checkIfWon(rappers, disses, rappersBeaten, won, beaten);
-		
+		checkIfWon(rappers, disses, rappersBeaten, won, beaten);
+
 	}
-	
-	void checkIfWon(Rapper[] rappers, Dis[] disses, int rappersBeaten, boolean won, boolean beaten) {
+
+	void checkIfWon(Rapper[] rappers, Dis[] disses, int rappersBeaten,
+			boolean won, boolean beaten) {
 		while (rappers[rappersBeaten].hypePoints > 0 && !won) {
 			playGameSP(rappers, disses, rappersBeaten);
 
 			if (rappers[rappersBeaten].hypePoints <= 0) {
 				System.out.println();
-				System.out.print("You beat "
-						+ rappers[rappersBeaten].name + "!");
+				System.out.print("You beat " + rappers[rappersBeaten].name
+						+ "!");
 				System.out.println();
 				System.out.println();
 				System.out.println();
-				
+
 				try {
 					Thread.sleep(4000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				
+
 				rappersBeaten++;
 				won = true;
 				singleplayer(rappers, disses, rappersBeaten);
@@ -197,9 +347,34 @@ public class Main {
 	}
 
 	void AITurn(Rapper[] rappers, Dis[] disses, int rappersBeaten) {
+		Random r = new Random();
+		int choice;
 		
+		choice = r.nextInt(3);
+		
+		System.out.println();
+		System.out.println("Now it's " + rappers[rappersBeaten].name + "'s turn");
+		System.out.println();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		switch (choice) {
+		case 0:
+			yourMumDisAI(rappers, disses, rappersBeaten);
+			break;
+		case 1:
+			callNameAI(rappers, disses, rappersBeaten);
+			break;
+		case 2:
+			teaseDisAI(rappers, disses, rappersBeaten);
+			break;
+		}
 	}
-	
+
 	public Main() {
 		Rapper[] rappers = new Rapper[6];
 		Dis[] disses = new Dis[4];
@@ -210,7 +385,7 @@ public class Main {
 
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		new Main();
 	}
 
